26bbc30a869fbf5ecb1f823f879e7809bee5e0d0
Colin Rood <crrood@gmail.com>
2013-12-24 13:39:35 -0800

    initial commit

---
+++ javascript.js
@@ -0,0 +1,48 @@
+
+var container;
+var NUM_LINES = 1500;
+var CONTAINER_WIDTH = window.innerWidth - 15;
+var CONTAINER_HEIGHT = window.innerHeight * (8/10);
+
+function init() {
+	container = document.getElementById("container");
+
+	container.style.width = CONTAINER_WIDTH + "px";
+	container.style.height = CONTAINER_HEIGHT + "px";
+
+	// outerChild is a mask to detect mouse events
+	// while innerChild is where the background actually changes
+	var outerChild, innerChild;
+	for (var i = 0; i < NUM_LINES; i++) {
+
+		outerChild = document.createElement("span");
+		outerChild.style.height = "100%";
+		outerChild.style.width = (100 / NUM_LINES) + "%";
+		outerChild.style.display = "inline-block";
+		outerChild.style.position = "relative";
+
+		outerChild.addEventListener("mousemove", mouseIsMoving);
+		outerChild.addEventListener("mouseout", mouseIsOut);
+
+		innerChild = document.createElement("span");
+		innerChild.style.background = "#" + (i%10) + (i%10) + (i%10);
+		innerChild.style.height = "0";
+		innerChild.style.width = "100%";
+		innerChild.style.display = "inline-block";
+		innerChild.style.position = "absolute";
+		innerChild.style.bottom = "0";
+		innerChild.style.transition = ".4s";
+
+		outerChild.appendChild(innerChild);
+		container.appendChild(outerChild);
+	}
+}
+
+function mouseIsMoving(e) {
+	this.childNodes.item(0).style.height = (CONTAINER_HEIGHT - e.clientY) + "px";
+}
+
+function mouseIsOut(e) {
+	var target = this;
+	setTimeout(function(e) { target.childNodes.item(0).style.height = "0%"; }, 600);
+}
